ba8938e4c8191d532b75425bd16e7d42d3ba1bd8
Zeus, The True God <l-2732@yandex.ru>
2011-06-20 21:22:57 +0400

    displaying a simple layouting issues

--- grid.js
+++ grid.js
@@ -1,8 +1,17 @@
 Ext.onReady(function() {
+  var grid1 = Ext.create('Ext.Panel', {
+    title: 'Grid placeholder',
+		html:'Some code <br/> with brakes inside'
+  });
+  var grid2 = Ext.create('Ext.Panel', {
+    title: 'Grid placeholder number 2',
+		height: 50
+  });
   new Ext.Panel({
     width: 400,
     height: 200,
     renderTo: 'place-for-grid',
-    title: 'Web Application Panel'
+    title: 'Web Application Panel',
+    items: [grid1, grid2]
   });
 });
