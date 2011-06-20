0642a95337c090ee9501177219606fc0cb49dbbd
Paul Reichow <Paul.Reichow@gmail.com>
2011-06-20 11:24:04 -0700

    this is an update

--- grid.js
+++ grid.js
@@ -1,8 +1,48 @@
 Ext.onReady(function() {
-  var grid1 = Ext.create('Ext.Panel', {
-    title: 'Grid placeholder',
-		html:'Some code <br/> with brakes inside'
-  });
+  Ext.define('User', {
+    extend: 'Ext.data.Model',
+    fields: [ 'name', 'email', 'phone' ]
+});
+
+var userStore = Ext.create('Ext.data.Store', {
+    model: 'User',
+    data: [
+        { name: 'Lisa', email: 'lisa@simpsons.com', phone: '555-111-1224' },
+        { name: 'Bart', email: 'bart@simpsons.com', phone: '555-222-1234' },
+        { name: 'Homer', email: 'home@simpsons.com', phone: '555-222-1244' },
+        { name: 'Marge', email: 'marge@simpsons.com', phone: '555-222-1254' }
+    ]
+});
+
+var grid1 = Ext.create('Ext.grid.Panel', {
+    forceFit: true,   
+    store: userStore,
+    
+    height: 100,
+    title: 'Application Users',
+    columns: [
+        {
+            text: 'Name',
+            width: 100,
+            sortable: true,
+            hideable: false,
+            dataIndex: 'name'
+        },
+        {
+            text: 'Email Address',
+            width: 150,
+            dataIndex: 'email',
+            hidden: true
+        },
+        {
+            text: 'Phone Number',
+            flex: 1,
+            dataIndex: 'phone'
+        }
+    ]
+});
+  
+    
   var grid2 = Ext.create('Ext.Panel', {
     title: 'Grid placeholder number 2',
 		height: 50
